19f5c30a59fdb5622775f9cc9398eb25aa503b01
euclid8128 <y.kato.534@ms.saitama-u.ac.jp>
2025-07-30 17:10:08 +0900

    Update unittest

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,6 @@ using Slit_Sim: potential
         hall_pos::Float64
         hall_number::Int
     end
-<<<<<<< HEAD
     L = 15
     N = 400 ##格子分割数
     dx = 2*L / N ##格子幅
@@ -32,13 +31,6 @@ using Slit_Sim: potential
         @test potential(1,1,L,dx, hall_params) == 0.0
     end
     
-=======
-    hall_size = 0.20    ##穴の半径
-    hall_pos  = 1.0    ##2つの穴の中央から穴の中心までの距離
-    hall_number = 3
-    hall_params = SlitConfig(hall_size, hall_pos, hall_number)
-    @test potential(1,1,10,0.1, hall_params) == 0.0
->>>>>>> cbd1d4a87b9781c7473a4b7d027a3616949c236f
 end
 
 nothing
