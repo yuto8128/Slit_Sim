cbd1d4a87b9781c7473a4b7d027a3616949c236f
wasaseki <makiharu192@gmail.com>
2025-07-30 16:52:46 +0900

    ユニットテストの変数名の修正

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ using Slit_Sim: potential, SlitConfig
     hall_pos  = 1.0    ##2つの穴の中央から穴の中心までの距離
     hall_number = 3
     hall_params = SlitConfig(hall_size, hall_pos, hall_number)
-    @test potential(1,1,10,0.1, hall_parames) == 0.0
+    @test potential(1,1,10,0.1, hall_params) == 0.0
 end
 
 nothing
