9fa3a8481e88fcb60af1c5754f395c663f4376e5
wasaseki <makiharu192@gmail.com>
2025-07-30 16:44:17 +0900

    ユニットテストの作成

---
+++ test/runtests.jl
@@ -0,0 +1,17 @@
+using Test
+using Slit_Sim: potential, SlitConfig
+
+@testset "potential" begin
+    struct SlitConfig
+        hall_size::Float64
+        hall_pos::Float64
+        hall_number::Int
+    end
+    hall_size = 0.20    ##穴の半径
+    hall_pos  = 1.0    ##2つの穴の中央から穴の中心までの距離
+    hall_number = 3
+    hall_params = SlitConfig(hall_size, hall_pos, hall_number)
+    @test potential(1,1,10,0.1, hall_parames) == 0.0
+end
+
+nothing
