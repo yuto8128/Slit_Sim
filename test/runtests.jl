848e8310e801c769e6c986b720f69ea8c58ca030
euclid8128 <y.kato.534@ms.saitama-u.ac.jp>
2025-07-30 17:06:15 +0900

    update unittest file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Test
-using Slit_Sim: potential, SlitConfig
+using Slit_Sim: potential
 
 @testset "potential" begin
     struct SlitConfig
@@ -7,11 +7,30 @@ using Slit_Sim: potential, SlitConfig
         hall_pos::Float64
         hall_number::Int
     end
-    hall_size = 0.20    ##穴の半径
-    hall_pos  = 1.0    ##2つの穴の中央から穴の中心までの距離
-    hall_number = 3
-    hall_params = SlitConfig(hall_size, hall_pos, hall_number)
-    @test potential(1,1,10,0.1, hall_parames) == 0.0
+    L = 15
+    N = 400 ##格子分割数
+    dx = 2*L / N ##格子幅
+
+    @testset "potential1" begin
+        hall_size = 0.20    ##穴の半径
+        hall_pos  = 2.0    ##2つの穴の中央から穴の中心までの距離
+        hall_number = 2
+        hall_params = SlitConfig(hall_size, hall_pos, hall_number)
+        
+
+        @test potential(1,1,L,dx, hall_params) == 0.0
+    end
+
+    @testset "potential2" begin
+        hall_size = 0.20    ##穴の半径
+        hall_pos  = 1.0    ##2つの穴の中央から穴の中心までの距離
+        hall_number = 3
+        hall_params = SlitConfig(hall_size, hall_pos, hall_number)
+        
+
+        @test potential(1,1,L,dx, hall_params) == 0.0
+    end
+    
 end
 
 nothing
